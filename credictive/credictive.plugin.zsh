46ac620cae9f5c4b8e353e974b6fd0ea38e727e3
Mariusz Lusiak <mariusz@applicake.com>
2013-01-02 10:28:22 +0100

    Adds nicer and credictive shortcuts

---
+++ credictive/credictive.plugin.zsh
@@ -0,0 +1,4 @@
+alias cdc='cd /Users/mariusz/Workspace/credictive/credictive'
+alias cdca='cd /Users/mariusz/Workspace/credictive/credictive/app'
+alias cdcs='cd /Users/mariusz/Workspace/credictive/credictive/scraper'
+alias cda='cd /Users/mariusz/Workspace/credictive/android-app'
---
+++ nicer/nicer.plugin.zsh
@@ -0,0 +1,3 @@
+alias cdn='cd /Users/mariusz/Workspace/nicer/app'
+alias cdna='cd /Users/mariusz/Workspace/nicer/android'
+alias cdni='cd /Users/mariusz/Workspace/nicer/ios'
