95fb88205af2980affd384fee4fdf193043fada8
Mariusz Lusiak <mariusz@getbase.com>
2013-12-09 21:08:43 +0100

    Bundler plugin replacement

---
+++ plugins/base/base.plugin.zsh
@@ -0,0 +1,5 @@
+alias b='cd /Users/mariusz/Projects/base/'
+
+# Alias for bundler. The Bundler plugin took too much to load, and this is
+# the only part of it I actually use
+alias be="bundle exec"
